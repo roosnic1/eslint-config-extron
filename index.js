324dc293595eb06a2a0e3704f44bf70152895f1b
Nicolas Roos <nicolas.k.roos@gmail.com>
2016-12-22 16:44:02 +0100

    improved package and added basic rule set

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 module.exports = {
 
-    globals: {
-        MyGlobal: true
-    },
-
-    rules: {
-        semi: [2, "always"]
-    }
+  "extends": "airbnb-base/legacy",
+  "env": {
+    "browser": true,
+    "jasmine": true,
+    "jquery": true
+  },
+  "globals": {
+    "angular": false,
+    "inject": false,
+    "getJSONFixture": false
+  },
+  "rules": {
+    "func-names": "off",
+    "max-len": "off",
+    "vars-on-top": "warn",
+    "one-var": "warn",
+    "no-undef": "warn",
+    "no-unused-vars": "warn"
+  }
 
 };
